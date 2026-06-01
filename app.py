e6e0a865bb3e6e7e84f5cce30f9d9e8a116c8fc1
phuc_offical <hpguc23612361@gmail.com>
2026-06-01 16:00:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ st.title("🎂 Chúc mừng ngày 1/6, người anh em!")
 if st.button('Nhấn vào đây để nhận quà bất ngờ'):
     st.balloons() # Hiệu ứng bóng bay
     st.success("Cảm ơn vì đã luôn là một người bạn tuyệt vời! Chúc bạn mọi điều tốt đẹp nhất!")
-    st.image("https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNHJqZ2h1dG56dG5qZzR4c3Z4ZzR4c3Z4ZzR4c3Z4ZzR4c3Z4JmVwPXYxX2ludGVybmFsX2dpZl9ieV9pZCZjdD1n/3o7TKMGpxxHOGTdzJC/giphy.gif")
+    st.video("https://youtube.com/shorts/PWy4sxLpl58?si=FGXBUhxSJgq9DxC3")
