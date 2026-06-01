7d1ede5acf24666b0a47b99e172748ee858895f4
phuc_offical <hpguc23612361@gmail.com>
2026-06-01 16:54:06 +0700

    Personalize success message and add new image
    Updated message to include 'em' for personalization and added a new image.

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ st.title("🎂 Chúc em ngày Quốc tế thiếu nhi thật nhiều niềm vui.
 
 if st.button('Nhấn vào đây để nhận quà bất ngờ'):
     st.balloons() # Hiệu ứng bóng bay
-    st.success("Cảm ơn vì đã luôn là một người bạn tuyệt vời! Chúc bạn mọi điều tốt đẹp nhất!")    
+    st.success("Cảm ơn vì đã luôn là một người bạn tuyệt vời! Chúc em mọi điều tốt đẹp nhất!")    
     st.image("anh.jpg", caption="Nhìn quen lắm đk")
+    st.image("ngoc_han_phongcanh.png",  caption="Nhìn quen lắm đk")
     st.video("https://www.youtube.com/watch?v=PWy4sxLpl58")
