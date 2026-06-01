5d46c6cdc55cfc03e6c3b96d1ba939d71fddcc41
phuc_offical <hpguc23612361@gmail.com>
2026-06-01 16:03:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import streamlit as st
-import time
 
 st.title("🎂 Chúc mừng ngày 1/6, người anh em!")
 
 if st.button('Nhấn vào đây để nhận quà bất ngờ'):
     st.balloons() # Hiệu ứng bóng bay
     st.success("Cảm ơn vì đã luôn là một người bạn tuyệt vời! Chúc bạn mọi điều tốt đẹp nhất!")
-    st.video("https://youtube.com/shorts/PWy4sxLpl58?si=FGXBUhxSJgq9DxC3")
+    
+    # Dòng này là chìa khóa để hiện video của bạn
+    st.video("https://www.youtube.com/watch?v=PWy4sxLpl58")
