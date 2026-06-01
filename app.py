dd74ad05d60b9c74e41588395cc8e71e763f057f
phuc_offical <hpguc23612361@gmail.com>
2026-06-01 15:52:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
+import streamlit as st
+import time
 
+st.title("🎂 Chúc mừng ngày 1/6, người anh em!")
+
+if st.button('Nhấn vào đây để nhận quà bất ngờ'):
+    st.balloons() # Hiệu ứng bóng bay
+    st.success("Cảm ơn vì đã luôn là một người bạn tuyệt vời! Chúc bạn mọi điều tốt đẹp nhất!")
+    st.image("https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNHJqZ2h1dG56dG5qZzR4c3Z4ZzR4c3Z4ZzR4c3Z4ZzR4c3Z4JmVwPXYxX2ludGVybmFsX2dpZl9ieV9pZCZjdD1n/3o7TKMGpxxHOGTdzJC/giphy.gif")
