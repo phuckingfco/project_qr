2546d8fe5df932a2df46c54dee5d8fd8810a5473
phuc_offical <hpguc23612361@gmail.com>
2026-06-01 17:10:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
 import streamlit as st
 
+# 1. Dán code ẩn giao diện vào đây (ngay dưới import)
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+st.set_page_config(layout="wide")
+
 st.title("🎂 Chúc em ngày Quốc tế thiếu nhi thật nhiều niềm vui. Dù lớn rồi nhưng vẫn luôn là một cô bé hạnh phúc nhé!")
 
 if st.button('Nhấn vào đây để nhận quà bất ngờ'):
